86ae4bd78f356866517ecd370890c5eab66f8a42
EmiCelis <emsace16@gmail.com>
2026-05-26 05:16:11 -0600

    Minor Changes

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class CanvasManager : MonoBehaviour
+{
+    public TextMeshProUGUI health;
+    public TextMeshProUGUI armor;
+    public TextMeshProUGUI weapon;
+
+    public Image healthIndicator;
+
+    public Image GearIndicator;
+
+    public GameObject redKey;
+    public GameObject blueKey;
+    public GameObject greenKey;
+}
